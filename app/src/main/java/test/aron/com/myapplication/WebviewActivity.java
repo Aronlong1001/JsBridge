6a7c51c4636e9bf7e6ef39e622c030bc010ec15f
aron <xiaolong.ji@bl.com>
2017-04-11 12:25:07 +0800

    Delete useless code

--- app/src/main/java/test/aron/com/myapplication/WebviewActivity.java
+++ app/src/main/java/test/aron/com/myapplication/WebviewActivity.java
@@ -1,48 +1,20 @@
 package test.aron.com.myapplication;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
 
-import com.bl.sdk.page.web.BLSBaseWebPage;
-import com.bl.sdk.page.web.WebViewJavascriptBridge;
 
 /**
  * Created by Aron on 2017/2/20.
  */
-public class WebviewActivity extends BLSBaseWebPage {
+public class WebviewActivity extends AppCompatActivity {
 
     private WebView mWebView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        Intent intent = new Intent();
-        intent.putExtra("pageId", "home");
-        setIntent(intent);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    protected void setupView() {
-        setContentView(R.layout.activity_web);
-        mWebView = (WebView) findViewById(R.id.basepage_webview);
-        mWebView.setWebContentsDebuggingEnabled(true);//chrome debug pattern
-    }
-
-    @Override
-    protected void setupNaviBar() {
-        //TODO:
-    }
-
-    @Override
-    protected void registerInterfaces(WebViewJavascriptBridge bridge) {
-        super.registerInterfaces(bridge);
-        bridge.registerHandler("handleTest", new WebViewJavascriptBridge.WVJBHandler() {
-            @Override
-            public void handle(String s, WebViewJavascriptBridge.WVJBResponseCallback wvjbResponseCallback) {
-                Log.e("handle: ", "skfasdflkasdjlkfjasdklfjasdfjlskdjfl");
-            }
-        });
     }
 }
